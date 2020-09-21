9c75f734972cb84c858a06a77c674b4cf38b2db6
prabuaddin30 <prabuaddin@student.telkomuniversity.ac.id>
2020-09-21 19:52:42 +0700

    soal1.java

---
+++ soal1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class soal1{
+    public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+	int x = (100000);
+	int y = (30);
+	int hasil = x * y;
+	System.out.println (3000000);
+    }
+}
+	
+	
---
+++ soal2.java
@@ -0,0 +1,6 @@
+public class soal2{
+  public static void main(String[] args){
+      System.out.println("Halo, Prabu");
+	  System.out.println("Ternyata usiamu baru 19 tahun ya!");
+  }
+}
