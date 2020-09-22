93f07d907ae63ee85b9ad266c748c147dfb464f8
prabuaddin30 <71245237+prabuaddin30@users.noreply.github.com>
2020-09-22 20:53:06 +0700

    Add files via upload

---
+++ soal3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class soal3{
+   public static void main (String[] args){
+   int x = (12);
+   int y = (3);
+   int hasil = x / (y+1);
+   int sisa = x % (y+1);
+   System.out.println(hasil);
+   System.out.println(sisa);
+   }
+}
+  
